65f90f3beeadef53bda8b962c9b90a648a46fea9
rdzee <arsil.anzil@gmail.com>
2022-09-18 18:53:24 +0700

    percobaan kedua

--- Kelompok D/Latihan.py
+++ Kelompok D/Latihan.py
@@ -1,1 +1,11 @@
-print("coba")
+#Kelompok D
+#Anggota:
+#1. Arsil Kultura Anzil
+#2. Muhammad Putra Yubiksana
+#3. Dede Ridwan
+
+i = int(input("Masukkan Nilai Anda : "))
+if(i < 60):
+    print("Nilai Anda C")
+else:
+    print("Anda Lulus")
