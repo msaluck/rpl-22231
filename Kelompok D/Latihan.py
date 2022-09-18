ede59d7130e3edba01fea90f06cad5adf9aa878a
Muhammad Putra <muhammad.yubiksana@mhs.unsoed.ac.id>
2022-09-18 20:34:39 +0700

    Menambahkan NIM M Putra dan menambah elif

--- Kelompok D/Latihan.py
+++ Kelompok D/Latihan.py
@@ -1,11 +1,15 @@
 #Kelompok D
 #Anggota:
 #1. Arsil Kultura Anzil (H1A020051)
-#2. Muhammad Putra Yubiksana
+#2. Muhammad Putra Yubiksana (H1A020059)
 #3. Dede Ridwan
 
 i = int(input("Masukkan Nilai Anda : "))
-if(i < 60):
-    print("Nilai Anda C")
+if(i <= 60):
+    print("Grade Nilai Anda C")
+    
+elif(61 <= i < 80):
+    print("Grade Nilai Anda B")
+    
 else:
     print("Anda Lulus")
