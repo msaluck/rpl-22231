1e9be05063b1585080c5c13e51f49cbe2eedd0d6
rdzee <arsil.anzil@gmail.com>
2022-09-18 20:39:28 +0700

    Menambahkan keterangan output pada kondisi if dan elif

--- Kelompok D/Latihan.py
+++ Kelompok D/Latihan.py
@@ -7,9 +7,9 @@
 i = int(input("Masukkan Nilai Anda : "))
 if(i <= 60):
     print("Grade Nilai Anda C")
-    
+    print("Anda Belum Lulus")
 elif(61 <= i < 80):
     print("Grade Nilai Anda B")
-    
+    print("Anda Sudah Lulus")
 else:
     print("Anda Lulus")
