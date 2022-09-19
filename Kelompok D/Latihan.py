77533af8ef7ba5298f2314d53eff8669b0b60fcb
rdzee <arsil.anzil@gmail.com>
2022-09-19 09:09:41 +0700

    Hasil akhir program

--- Kelompok D/Latihan.py
+++ Kelompok D/Latihan.py
@@ -4,12 +4,15 @@
 #2. Arsil Kultura Anzil (H1A20051)
 #3. Muhammad Putra Yubiksana (H1A020059)
 
-i = int(input("Masukkan Nilai Anda : "))
+i = int(input("Masukan Nilai Anda: "))
 if(i <= 60):
     print("Grade Nilai Anda C")
     print("Anda Belum Lulus")
 elif(61 <= i < 80):
     print("Grade Nilai Anda B")
     print("Anda Sudah Lulus")
+elif(i >= 80):
+    print("Grade Nilai Anda A")
+    print("Anda Lulus Dengan Hasil Memuaskan")
 else:
     print("Anda Salah Memasukan Input")
